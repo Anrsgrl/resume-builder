9e08107136d70988eb8ef5456d361894e0e7d3f5
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-08 13:08:30 +0400

    Feature: Custom textarea component created

--- app/[locale]/build/page.jsx
+++ app/[locale]/build/page.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+import Input from "@/components/common/Input";
+import TextArea from "@/components/common/TextArea";
+import React, { useState } from "react";
 
 const Build = () => {
-  return <div>Build</div>;
+  const [name, setName] = useState("");
+  return (
+    <div className="mt-20 px-10">
+      <Input state={name} setState={setName} label="Name" name="name" />
+    </div>
+  );
 };
 
 export default Build;
---
+++ components/common/TextArea.jsx
@@ -0,0 +1,24 @@
+const TextArea = ({ state, setState, label, name }) => {
+  return (
+    <div class="relative">
+      <textarea
+        type="text"
+        name={name}
+        id={name}
+        className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-1 appearance-none text-white border-gray-600 dark:focus:border-main focus:outline-none focus:ring-0 focus:border-main peer border min-h-32"
+        placeholder=" "
+        onChange={(e) => setState(e.target.value)}
+      />
+      <label
+        for={name}
+        className={`absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] peer-focus:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-main peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-6 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1 ${
+          state !== "" && "bg-gray-900 text-main"
+        }`}
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default TextArea;
