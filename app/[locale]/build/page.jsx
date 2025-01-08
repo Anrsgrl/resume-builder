83c60be838bfae8bd7ecd27915b3901393063826
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-08 13:23:59 +0400

    Feature: Custom button component created

--- app/[locale]/build/page.jsx
+++ app/[locale]/build/page.jsx
@@ -1,13 +1,16 @@
 "use client";
+import Button from "@/components/common/Button";
 import Input from "@/components/common/Input";
-import TextArea from "@/components/common/TextArea";
 import React, { useState } from "react";
 
 const Build = () => {
   const [name, setName] = useState("");
   return (
-    <div className="mt-20 px-10">
+    <div className="mt-20 px-10 flex flex-col gap-2">
       <Input state={name} setState={setName} label="Name" name="name" />
+      <Button onClick={() => alert("namne")} animation={true}>
+        Name
+      </Button>
     </div>
   );
 };
---
+++ components/common/Button.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MdArrowRightAlt } from "react-icons/md";
+
+const Button = ({ children, animation, icon, ...props }) => {
+  return (
+    <button
+      {...props}
+      type="button"
+      className="w-fit flex items-center gap-1 animation-all group overflow-hidden w-fit px-3 py-1 rounded-lg text-main cursor-pointer border border-main hover:border-main hover:bg-main group font-semibold"
+    >
+      {animation && (
+        <span className="transform -translate-x-5 animation-all group-hover:translate-x-0 opacity-0 group-hover:opacity-100 group-hover:text-white ">
+          <MdArrowRightAlt size={24} />
+        </span>
+      )}
+      <p
+        className={`uppercase text-sm animation-all group-hover:text-white ${
+          animation && "-translate-x-3 group-hover:translate-x-0"
+        }`}
+      >
+        {children}
+      </p>
+    </button>
+  );
+};
+
+export default Button;
