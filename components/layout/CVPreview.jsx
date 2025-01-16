320883eb6e2bd8f62053ba7e350dc43231ad6ce5
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-17 01:24:12 +0400

    Refactor: Save function changed

--- components/layout/CVPreview.jsx
+++ components/layout/CVPreview.jsx
@@ -1,33 +1,9 @@
 "use client";
-import { jsPDF } from "jspdf";
-import { useState } from "react";
 import Template1 from "@/components/templates/Template1";
+
 const CVPreview = () => {
-  const [loading, setLoading] = useState(false);
-
-  const handleDownloadPDF = () => {
-    setLoading(true);
-    try {
-      const pdf = new jsPDF("p", "px", "a4");
-      const content = document.getElementById("template1");
-
-      pdf.html(content, {
-        callback: function (doc) {
-          doc.save("CV.pdf");
-        },
-        x: 10,
-        y: 10,
-        autoPaging: "slice",
-        html2canvas: {
-          scale: 0.7,
-          useCORS: true,
-        },
-      });
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      console.error(error);
-    }
+  const handlePrint = () => {
+    window.print();
   };
 
   return (
@@ -35,10 +11,10 @@ const CVPreview = () => {
       <Template1 />
       <div className="mt-4 text-center">
         <button
-          onClick={handleDownloadPDF}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={handlePrint}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 print:hidden"
         >
-          {loading ? "Saving..." : "Save Resume as PDF"}
+          Save Resume as PDF
         </button>
       </div>
     </>
