90db4e0d3ba6d7bfe3fba10ef7704cca0549d8d6
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-16 14:20:17 +0400

    Refactor: Format time function modified for locale

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,16 +1,9 @@
 //* Create formatted time
-export const useFormattedTime = (time) => {
+export const useFormattedTime = (time, locale) => {
   if (!time) return "";
-  let date;
-  if (typeof time.toDate === "function") {
-    date = time.toDate();
-  } else if (typeof time === "string" || time instanceof Date) {
-    date = new Date(time);
-  } else {
-    throw new Error("Invalid date format");
-  }
-  const options = { day: "2-digit", month: "short", year: "numeric" };
-  const formattedDate = date.toLocaleDateString("en-US", options);
+  const date = new Date(time);
+  const options = { month: "short", year: "numeric" };
+  const formattedDate = date.toLocaleDateString(locale || "en-US", options);
 
   return formattedDate;
 };
