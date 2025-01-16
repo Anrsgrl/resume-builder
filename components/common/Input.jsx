f5751ba840ab7452e4ed02488e797499d7add1c0
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-16 14:19:35 +0400

    Refactor: Input disable button added
    Added because some parts needs to be empty like end date

--- components/common/Input.jsx
+++ components/common/Input.jsx
@@ -1,4 +1,11 @@
-const Input = ({ state, setState, label, name, type = "text" }) => {
+import { useState } from "react";
+import { FaCalendarTimes } from "react-icons/fa";
+const Input = ({ state, setState, label, name, type = "text", present }) => {
+  const [presentStatus, setPresentStatus] = useState(false);
+  const setPresent = () => {
+    setState("");
+    setPresentStatus(!presentStatus);
+  };
   return (
     <div className="relative">
       <input
@@ -6,18 +13,30 @@ const Input = ({ state, setState, label, name, type = "text" }) => {
         name={name}
         value={state}
         id={name}
-        className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-1 appearance-none text-white border-gray-600 dark:focus:border-main focus:outline-none focus:ring-0 focus:border-main peer border"
+        className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-1 appearance-none text-white border-gray-600 dark:focus:border-main focus:outline-none focus:ring-0 focus:border-main peer border disabled:opacity-50"
         placeholder=" "
+        disabled={presentStatus}
         onChange={(e) => setState(e.target.value)}
       />
       <label
         title={state}
         htmlFor={name}
-        className={`absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] peer-focus:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-main peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1 ${
+        className={`absolute flex items-center gap-1 text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] peer-focus:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-main peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1 ${
           (state !== "" || state?.length > 0) && "bg-gray-900 text-main"
         }`}
       >
-        {label}
+        {label}{" "}
+        {present && (
+          <button
+            type="button"
+            className={`bg-gray-800 px-2 rounded-lg flex items-center gap-1 ${
+              presentStatus ? "text-main" : "text-gray-400"
+            }`}
+            onClick={setPresent}
+          >
+            Present <FaCalendarTimes />
+          </button>
+        )}
       </label>
     </div>
   );
