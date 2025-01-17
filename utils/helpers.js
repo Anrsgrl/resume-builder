e539d01d84713f752d0df1bfad1eac37b35fb3c3
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-17 18:09:53 +0400

    Fix: toLocaleDateString az-AZ problem fixed

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,30 @@
+const azMonths = [
+  "Yan",
+  "Fev",
+  "Mar",
+  "Apr",
+  "May",
+  "İyn",
+  "İyl",
+  "Avq",
+  "Sen",
+  "Okt",
+  "Noy",
+  "Dek",
+];
+//* toLocaleDateString does not support the Azerbaijani language, I should have written like this
+
 //* Create formatted time
 export const useFormattedTime = (time, locale) => {
   if (!time) return "";
   const date = new Date(time);
+
+  if (locale === "az-AZ") {
+    const month = azMonths[date.getMonth()];
+    const year = date.getFullYear();
+    return `${month} ${year}`;
+  }
+
   const options = { month: "short", year: "numeric" };
   const formattedDate = date.toLocaleDateString(locale || "en-US", options);
 
