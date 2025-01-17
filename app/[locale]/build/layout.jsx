c89b20cd215e0ec04a92ba3eec7b2b14d65ec510
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-17 14:42:26 +0400

    Refactor: Resume review mobile mode added

--- app/[locale]/build/layout.jsx
+++ app/[locale]/build/layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const BuildLayout = ({ children }) => {
   return (
-    <div className="grid grid-cols-2 print:grid-cols-1">
+    <div className="grid grid-cols-1 xl:grid-cols-2 print:grid-cols-1">
       <div className="print:hidden">{children}</div>
       <div className="border-l border-main ps-4 mt-4 print:border-0 print:p-0 print:m-0">
         <CVPreview />
--- components/layout/CVPreview.jsx
+++ components/layout/CVPreview.jsx
@@ -1,22 +1,58 @@
 "use client";
 import Template1 from "@/components/templates/Template1";
-
+import { useState } from "react";
+import { FaWindowMinimize } from "react-icons/fa";
+import { PiReadCvLogo } from "react-icons/pi";
+import { FiMinimize } from "react-icons/fi";
 const CVPreview = () => {
   const handlePrint = () => {
     window.print();
   };
+  const [show, setShow] = useState(false);
 
+  const openReview = () => {
+    setShow(!show);
+  };
   return (
     <>
-      <div className="mb-2 text-center">
-        <button
-          onClick={handlePrint}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 print:hidden"
-        >
-          Save Resume as PDF
-        </button>
+      <button
+        onClick={openReview}
+        type="button"
+        className="fixed bottom-4 right-4 w-16 h-16 bg-main hover:bg-main/90 animation-all rounded-full flex items-center justify-center group/mobile xl:hidden print:hidden cursor-pointer z-50"
+      >
+        {show ? (
+          <FiMinimize
+            size={36}
+            color="white"
+            className="group-hover/mobile:rotate-[360deg] group-hover/mobile:scale-95 animation-all"
+          />
+        ) : (
+          <PiReadCvLogo
+            size={36}
+            color="white"
+            className="group-hover/mobile:rotate-[360deg] group-hover/mobile:scale-95 animation-all"
+          />
+        )}
+      </button>
+      <div
+        className={`flex flex-col items-center justify-center  print:h-max ${
+          show
+            ? "absolute inset-0 z-40 overflow-visible py-4 print:py-0 bg-black/80 h-[100dvh]"
+            : "hidden xl:block print:block h-[95dvh]"
+        }`}
+      >
+        <div className="mb-2 text-center">
+          <button
+            onClick={handlePrint}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 print:hidden"
+          >
+            Save Resume as PDF
+          </button>
+        </div>
+        <div className="w-full max-h-[90dvh] overflow-auto print:overflow-visible scale-75 lg:scale-100 print:scale-100">
+          <Template1 />
+        </div>
       </div>
-      <Template1 />
     </>
   );
 };
