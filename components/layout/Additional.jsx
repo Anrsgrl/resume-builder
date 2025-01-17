4d4e3dad8cbc312e6a0f98a1933501445e12fa78
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-17 19:05:37 +0400

    Refactor: i18n support for Additional informations input field added

--- components/layout/Additional.jsx
+++ components/layout/Additional.jsx
@@ -4,12 +4,14 @@ import Interests from "@/components/shared/Interests";
 import References from "@/components/shared/References";
 import Languages from "@/components/shared/Languages";
 import Stepper from "@/components/shared/Stepper";
+import { useTranslations } from "next-intl";
 
 const Additional = () => {
+  const t = useTranslations("Additional");
   return (
     <div className="mt-20 px-10 flex flex-col gap-2">
       <h1 className="text-center font-bold text-3xl text-main mb-4">
-        Additional Informations
+        {t("title")}
       </h1>
       <Certificates />
       <Interests />
