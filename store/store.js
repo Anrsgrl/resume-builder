e86abce25dc63a47409c5d41178d2ff25d8edd9a
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-21 22:25:29 +0400

    Refactor: Stores optimized!

--- store/store.js
+++ store/store.js
@@ -4,236 +4,113 @@ import { persist } from "zustand/middleware";
 const useStore = create(
   persist(
     (set) => ({
-      //! General
-      name: "",
-      surname: "",
-      email: "",
-      phone: "",
-      jobTitle: "",
-      country: "",
-      city: "",
-      driving: "",
-      setName: (value) => set({ name: value }),
-      setSurname: (value) => set({ surname: value }),
-      setEmail: (value) => set({ email: value }),
-      setPhone: (value) => set({ phone: value }),
-      setJobTitle: (value) => set({ jobTitle: value }),
-      setDriving: (value) => set({ driving: value }),
-      setCountry: (value) => set({ country: value }),
-      setCity: (value) => set({ city: value }),
+      store: {
+        //! General
+        general: {
+          name: "",
+          surname: "",
+          email: "",
+          phone: "",
+          jobTitle: "",
+          country: "",
+          city: "",
+          driving: "",
+        },
 
-      //! Social
-      socialLinks: {
-        linkedin: "",
-        github: "",
-        twitter: "",
-        facebook: "",
-        instagram: "",
-        website: "",
-        xing: "",
-        medium: "",
-        figma: "",
-        dribbble: "",
+        //! Social
+        socialLinks: {
+          linkedin: "",
+          github: "",
+          twitter: "",
+          facebook: "",
+          instagram: "",
+          website: "",
+          xing: "",
+          medium: "",
+          figma: "",
+          dribbble: "",
+        },
+
+        //! Photo
+        image: null,
+
+        //! Summary
+        summary: "",
+
+        //! Certificates
+        certificates: [],
+
+        //! Experiences
+        experience: [],
+
+        //! Languages
+        languages: [],
+
+        //! Education
+        education: [],
+
+        //! Skills
+        skills: [],
+
+        //! Projects
+        projects: [],
+
+        //! Interests
+        interests: [],
+
+        //! References
+        references: [],
       },
-      setLinkedin: (url) =>
+
+      //! Generic setter
+      setStore: (key, value) =>
+        set((state) => {
+          const keys = key.split(".");
+          const lastKey = keys.pop();
+          let nestedState = state.store;
+
+          keys.forEach((k) => {
+            nestedState = nestedState[k];
+          });
+
+          nestedState[lastKey] = value;
+          return { store: { ...state.store } };
+        }),
+
+      //! Add, remove, edit, reorder utilities
+      addItem: (section, newItem) =>
         set((state) => ({
-          socialLinks: { ...state.socialLinks, linkedin: url },
-        })),
-      setGithub: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, github: url },
-        })),
-      setTwitter: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, twitter: url },
-        })),
-      setFacebook: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, facebook: url },
-        })),
-      setInstagram: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, instagram: url },
-        })),
-      setWebsite: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, website: url },
-        })),
-      setXing: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, xing: url },
-        })),
-      setMedium: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, medium: url },
-        })),
-      setFigma: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, figma: url },
-        })),
-      setDribbble: (url) =>
-        set((state) => ({
-          socialLinks: { ...state.socialLinks, dribbble: url },
+          store: {
+            ...state.store,
+            [section]: [...state.store[section], newItem],
+          },
         })),
 
-      //! Photo
-      image: null,
-      setImage: (imageBase64) => set({ image: imageBase64 }),
+      removeItem: (section, index) =>
+        set((state) => ({
+          store: {
+            ...state.store,
+            [section]: state.store[section].filter((_, i) => i !== index),
+          },
+        })),
 
-      //! Summary
-      summary: "",
-      setSummary: (newSummary) => set({ summary: newSummary }),
+      editItem: (section, index, updatedItem) =>
+        set((state) => ({
+          store: {
+            ...state.store,
+            [section]: state.store[section].map((item, i) =>
+              i === index ? updatedItem : item
+            ),
+          },
+        })),
 
-      //! Certificates
-      certificates: [],
-      addCertificate: (newCertificate) =>
+      updateOrder: (section, updatedItems) =>
         set((state) => ({
-          certificates: [...state.certificates, newCertificate],
+          store: {
+            ...state.store,
+            [section]: updatedItems,
+          },
         })),
-      removeCertificate: (index) =>
-        set((state) => ({
-          certificates: state.certificates.filter((_, i) => i !== index),
-        })),
-      editCertificate: (index, updatedCertificate) =>
-        set((state) => ({
-          certificates: state.certificates.map((cert, i) =>
-            i === index ? updatedCertificate : cert
-          ),
-        })),
-      updateCertificatesOrder: (updatedCertificates) =>
-        set({ certificates: updatedCertificates }),
-
-      //! Experiences
-      experience: [],
-      addExperience: (newExperience) =>
-        set((state) => ({
-          experience: [...state.experience, newExperience],
-        })),
-      removeExperience: (index) =>
-        set((state) => ({
-          experience: state.experience.filter((_, i) => i !== index),
-        })),
-      editExperience: (index, updatedExperience) =>
-        set((state) => ({
-          experience: state.experience.map((exp, i) =>
-            i === index ? updatedExperience : exp
-          ),
-        })),
-      updateExperienceOrder: (updatedExperience) =>
-        set({ experience: updatedExperience }),
-
-      //! Languages
-      languages: [],
-      addLanguage: (newLanguage) =>
-        set((state) => ({
-          languages: [...state.languages, newLanguage],
-        })),
-      removeLanguage: (index) =>
-        set((state) => ({
-          languages: state.languages.filter((_, i) => i !== index),
-        })),
-      editLanguage: (index, updatedLanguage) =>
-        set((state) => ({
-          languages: state.languages.map((lang, i) =>
-            i === index ? updatedLanguage : lang
-          ),
-        })),
-      updateLanguagesOrder: (updatedLanguage) =>
-        set({ languages: updatedLanguage }),
-
-      //! Education
-      education: [],
-      addEducation: (newEducation) =>
-        set((state) => ({
-          education: [...state.education, newEducation],
-        })),
-      removeEducation: (index) =>
-        set((state) => ({
-          education: state.education.filter((_, i) => i !== index),
-        })),
-      editEducation: (index, updatedEducation) =>
-        set((state) => ({
-          education: state.education.map((edu, i) =>
-            i === index ? updatedEducation : edu
-          ),
-        })),
-      updateEducationOrder: (updatedEducation) =>
-        set({ education: updatedEducation }),
-
-      //! Skills
-      skills: [],
-      addSkill: (newSkill) =>
-        set((state) => ({
-          skills: [...state.skills, newSkill],
-        })),
-      removeSkill: (index) =>
-        set((state) => ({
-          skills: state.skills.filter((_, i) => i !== index),
-        })),
-      editSkill: (index, updatedSkill) =>
-        set((state) => ({
-          skills: state.skills.map((skill, i) =>
-            i === index ? updatedSkill : skill
-          ),
-        })),
-      updateSkillsOrder: (updatedSkills) => set({ skills: updatedSkills }),
-
-      //! Projects
-      projects: [],
-      addProject: (newProject) =>
-        set((state) => ({
-          projects: [...state.projects, newProject],
-        })),
-      removeProject: (index) =>
-        set((state) => ({
-          projects: state.projects.filter((_, i) => i !== index),
-        })),
-      editProject: (index, updatedProject) =>
-        set((state) => ({
-          projects: state.projects.map((proj, i) =>
-            i === index ? updatedProject : proj
-          ),
-        })),
-      updateProjectOrder: (updatedProjects) =>
-        set({ projects: updatedProjects }),
-
-      //! Interests
-      interests: [],
-      addInterest: (newInterest) =>
-        set((state) => ({
-          interests: [...state.interests, newInterest],
-        })),
-      removeInterest: (index) =>
-        set((state) => ({
-          interests: state.interests.filter((_, i) => i !== index),
-        })),
-      editInterest: (index, updatedInterest) =>
-        set((state) => ({
-          interests: state.interests.map((interest, i) =>
-            i === index ? updatedInterest : interest
-          ),
-        })),
-      updateInterestsOrder: (updatedInterests) =>
-        set({ interests: updatedInterests }),
-
-      //! References
-      references: [],
-      addReference: (newReference) =>
-        set((state) => ({
-          references: [...state.references, newReference],
-        })),
-      removeReference: (index) =>
-        set((state) => ({
-          references: state.references.filter((_, i) => i !== index),
-        })),
-      editReference: (index, updatedReference) =>
-        set((state) => ({
-          references: state.references.map((ref, i) =>
-            i === index ? updatedReference : ref
-          ),
-        })),
-      updateReferencesOrder: (updatedReference) =>
-        set({ references: updatedReference }),
     }),
     {
       name: "resume-data",
--- store/template.js
+++ store/template.js
@@ -4,53 +4,43 @@ import { persist } from "zustand/middleware";
 const useTemplateStore = create(
   persist(
     (set) => ({
-      //! Main
-      name: "CV",
-      templateNumber: 1,
-      setName: (value) => set({ name: value }),
-      setTemplateNumber: (value) => set({ templateNumber: value }),
+      template: {
+        //! Main
+        name: "CV",
+        templateNumber: 1,
 
-      //! Colors
-      h1Color: "",
-      h2Color: "",
-      h3Color: "",
-      textColor: "",
-      descriptionColor: "",
-      hyperLinkColor: "",
-      seth1Color: (value) => set({ h1Color: value }),
-      seth2Color: (value) => set({ h2Color: value }),
-      seth3Color: (value) => set({ h3Color: value }),
-      setTextColor: (value) => set({ textColor: value }),
-      setDescriptionColor: (value) => set({ descriptionColor: value }),
-      setHyperLinkColor: (value) => set({ hyperLinkColor: value }),
+        //! Colors
+        h1Color: "",
+        h2Color: "",
+        h3Color: "",
+        textColor: "",
+        descriptionColor: "",
+        hyperLinkColor: "",
 
-      //! Section
-      imageSize: "",
-      projectLink: "",
-      spaceBetween: "",
-      h2Align: "",
-      titleCase: "",
-      setImageSize: (value) => set({ imageSize: value }),
-      setProjectLink: (value) => set({ projectLink: value }),
-      setSpaceBetween: (value) => set({ spaceBetween: value }),
-      seth2Align: (value) => set({ h2Align: value }),
-      setTitleCase: (value) => set({ titleCase: value }),
+        //! Section
+        imageSize: "",
+        projectLink: "",
+        spaceBetween: "",
+        h2Align: "",
+        titleCase: "",
 
-      //! Font
-      fontFamily: "",
-      h1FontSize: "",
-      h2FontSize: "",
-      h3FontSize: "",
-      textFontSize: "",
-      descriptionFontSize: "",
-      hyperLinkFontSize: "#0284c7",
-      setFontFamily: (value) => set({ fontFamily: value }),
-      seth1FontSize: (value) => set({ h1FontSize: value }),
-      seth2FontSize: (value) => set({ h2FontSize: value }),
-      seth3FontSize: (value) => set({ h3FontSize: value }),
-      setTextFontSize: (value) => set({ textFontSize: value }),
-      setDescriptionFontSize: (value) => set({ descriptionFontSize: value }),
-      setHyperLinkFontSize: (value) => set({ hyperLinkFontSize: value }),
+        //! Font
+        fontFamily: "",
+        h1FontSize: "",
+        h2FontSize: "",
+        h3FontSize: "",
+        textFontSize: "",
+        descriptionFontSize: "",
+        hyperLinkFontSize: "#0284c7",
+      },
+
+      setTemplate: (key, value) =>
+        set((state) => ({
+          template: {
+            ...state.template,
+            [key]: value,
+          },
+        })),
     }),
     {
       name: "template",
