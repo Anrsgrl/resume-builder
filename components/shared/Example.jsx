45dd54086297e22b47f9784faf465f50089506dd
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-22 00:43:14 +0400

    Style: Example field style changed

--- components/shared/Example.jsx
+++ components/shared/Example.jsx
@@ -1,53 +1,64 @@
 import { useTranslations } from "next-intl";
 import { FaArrowDown, FaArrowUp, FaEdit, FaTrash } from "react-icons/fa";
 import { TbClick } from "react-icons/tb";
+import Menu from "./settings/Menu";
+import { PiDotsThreeOutlineVerticalFill } from "react-icons/pi";
 
 const Example = ({ children, index, state, title, edit, remove, up, down }) => {
   const t = useTranslations("General");
   return (
     <details
       key={index}
-      className="border border-white/50 px-4 py-2 rounded-md animation-all mt-2 text-sm group"
+      className="bg-zinc-700/10 border-2 border-zinc-600 text-white hover:bg-zinc-700/20 px-4 py-2 rounded-md animation-all mt-2 text-sm group"
     >
       <summary className="font-bold text-white/80 flex items-center justify-between cursor-pointer text-base">
         <span className="flex items-center gap-1">
           <span className="max-w-[90%] truncate text-sm">{title}</span>{" "}
           <TbClick className={`group-hover:text-main animation-all`} />
         </span>
-        {up && down && (
-          <div className="flex items-center gap-2">
+        <div className="flex items-center gap-1">
+          {up && down && (
+            <div className="flex items-center gap-2">
+              <button
+                onClick={() => up(index)}
+                disabled={index === 0}
+                className="disabled:opacity-50 disabled:cursor-not-allowed hover:scale-90 disabled:hover:scale-100 animation-all"
+              >
+                <FaArrowUp size={16} />
+              </button>
+              <button
+                onClick={() => down(index)}
+                disabled={index === state.length - 1}
+                className="disabled:opacity-50 disabled:cursor-not-allowed hover:scale-90 disabled:hover:scale-100 animation-all"
+              >
+                <FaArrowDown size={16} />
+              </button>
+            </div>
+          )}
+          <Menu
+            icon={<PiDotsThreeOutlineVerticalFill />}
+            className__button={`bg-transparent border-0`}
+          >
             <button
-              onClick={() => up(index)}
-              disabled={index === 0}
-              className="disabled:opacity-50 disabled:cursor-not-allowed hover:scale-90 disabled:hover:scale-100 animation-all"
+              type="button"
+              onClick={() => edit(index)}
+              className="px-4 py-2 text-sm flex items-center justify-between hover:bg-zinc-700 cursor-pointer rounded-md animation-all w-full font-normal text-blue-400"
             >
-              <FaArrowUp size={16} />
+              {t("edit")}
+              <FaEdit />
             </button>
             <button
-              onClick={() => down(index)}
-              disabled={index === state.length - 1}
-              className="disabled:opacity-50 disabled:cursor-not-allowed hover:scale-90 disabled:hover:scale-100 animation-all"
+              type="button"
+              onClick={() => remove(index)}
+              className="px-4 py-2 text-sm flex items-center justify-between hover:bg-zinc-700 cursor-pointer rounded-md animation-all w-full font-normal text-red-400"
             >
-              <FaArrowDown size={16} />
+              {t("remove")}
+              <FaTrash />
             </button>
-          </div>
-        )}
+          </Menu>
+        </div>
       </summary>
       {children}
-      <div className="flex items-center gap-2 mt-2 pt-2 border-t">
-        <button
-          onClick={() => edit(index)}
-          className="text-blue-500 hover:text-blue-700 p-2 rounded-full animation-all flex items-center gap-1 text-xs"
-        >
-          <FaEdit size={16} /> {t("edit")}
-        </button>
-        <button
-          onClick={() => remove(index)}
-          className="text-red-500 hover:text-red-700 p-2 rounded-full animation-all flex items-center gap-1 text-xs"
-        >
-          <FaTrash size={16} /> {t("remove")}
-        </button>
-      </div>
     </details>
   );
 };
