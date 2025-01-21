48b036f26a0006527516190585282956b35f8ab4
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-21 19:28:27 +0400

    Refactor: New menu items added to store

--- store/template.js
+++ store/template.js
@@ -4,20 +4,51 @@ import { persist } from "zustand/middleware";
 const useTemplateStore = create(
   persist(
     (set) => ({
+      //! Main
       name: "CV",
       templateNumber: 1,
-      sectionHeadingColor: "",
-      headingColor: "",
-      hyperlinkColor: "",
-      projectLink: "",
-      fontFamily: "",
       setName: (value) => set({ name: value }),
       setTemplateNumber: (value) => set({ templateNumber: value }),
-      setSectionHeadingColor: (value) => set({ sectionHeadingColor: value }),
-      setHeadingColor: (value) => set({ headingColor: value }),
-      setHyperlinkColor: (value) => set({ hyperlinkColor: value }),
+
+      //! Colors
+      h1Color: "",
+      h2Color: "",
+      h3Color: "",
+      textColor: "",
+      descriptionColor: "",
+      hyperLinkColor: "",
+      seth1Color: (value) => set({ h1Color: value }),
+      seth2Color: (value) => set({ h2Color: value }),
+      seth3Color: (value) => set({ h3Color: value }),
+      setTextColor: (value) => set({ textColor: value }),
+      setDescriptionColor: (value) => set({ descriptionColor: value }),
+      setHyperLinkColor: (value) => set({ hyperLinkColor: value }),
+
+      //! Section
+      imageSize: "",
+      projectLink: "",
+      spaceBetween: "",
+      h2Align: "",
+      setImageSize: (value) => set({ imageSize: value }),
       setProjectLink: (value) => set({ projectLink: value }),
+      setSpaceBetween: (value) => set({ spaceBetween: value }),
+      seth2Align: (value) => set({ h2Align: value }),
+
+      //! Font
+      fontFamily: "",
+      h1FontSize: "",
+      h2FontSize: "",
+      h3FontSize: "",
+      textFontSize: "",
+      descriptionFontSize: "",
+      hyperLinkFontSize: "",
       setFontFamily: (value) => set({ fontFamily: value }),
+      seth1FontSize: (value) => set({ h1FontSize: value }),
+      seth2FontSize: (value) => set({ h2FontSize: value }),
+      seth3FontSize: (value) => set({ h3FontSize: value }),
+      setTextFontSize: (value) => set({ textFontSize: value }),
+      setDescriptionFontSize: (value) => set({ descriptionFontSize: value }),
+      setHyperLinkFontSize: (value) => set({ hyperLinkFontSize: value }),
     }),
     {
       name: "template",
